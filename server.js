b248a2ba362759240c56537e6831e96fed6a7ad1
Mark Maksi <markmaksi@outlook.com>
2022-06-10 08:16:02 +0300

    synchronizing the ball and score between the client and the opponent

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ io.on("connection", (socket) => {
   });
 
   socket.on("paddleMove", (paddleData) => {
-    socket.broadcast.emit("paddleMove", paddleData)
-  })
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 });
